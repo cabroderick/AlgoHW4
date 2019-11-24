98e98eb73632dd045f2534d54fd393d37984d1a0
Collin Broderick <cabroderick@wpi.edu>
2019-11-23 21:06:59 -0500

    Worked on elimination algorithm to fix issue

--- AlgoHW4/AlgoHW4/AlgoHW4.cpp
+++ AlgoHW4/AlgoHW4/AlgoHW4.cpp
@@ -20,40 +20,56 @@ using namespace std;
 #include <malloc.h>
 #include <vector>
 #include <iostream>
-vector< vector<int> > Elimination (vector<vector<int> > A, vector<int> b) {
-	int n = A.size(); // The dimensions of the matrix
-	for (int i = 0; i < A.size(); i++) {
+vector< vector<double> > Elimination (vector<vector<double> > A, vector<double> b) {
+	double n = A.size(); // The rows of the matrix
+	for (int i = 0; i < n; i++) {
 		A.at(i).push_back(b.at(i));
 	}
 	for (int i = 0; i < n - 1; i++) {
-		int pivotrow = i;
-		for (int j = i + 1; j < n; j++) {
-			if (A[j][i] > A[pivotrow][i]) pivotrow = j;
-		}
-		for (int k = i; k < n + 1; k++) {
-			int temp1 = A[i][k]; // ERROR OCCURS HERE
-			int temp2 = A[pivotrow][k];
-			A[i][k] = temp2;
-			A[pivotrow][k] = temp1;
-		}
-		for (int j = i + 1; j < n; j++) {
-			int temp = A[j][i] / A[i][i];
-			for (int k = i; k < n + 1; k++) {
-				A[j][k] = A[j][k] - A[i][k] * temp;
+		bool solved = false;
+		int count = 0;
+		for (int j = n - 1; j > 0; j--) {
+			if (A[j][i] == 0) {
+				count++;
 			}
+			else break;
+		}
+		if (count > n - 2) solved = true;
+		if (!solved) {
+			double pivotrow = i;
+			for (int j = i + 1; j < n; j++) {
+				if (A[j][i] > A[pivotrow][i]) pivotrow = j;
+			}
+			for (int k = i; k < n + 1; k++) {
+				double temp1 = A[i][k];
+				double temp2 = A[pivotrow][k];
+				A[i][k] = temp2;
+				A[pivotrow][k] = temp1;
+			}
+			for (int j = i + 1; j < n; j++) {
+				double temp = (double)A[j][i] / (double)A[i][i];
+				for (double k = i; k < n + 1; k++) {
+					A[j][k] = A[j][k] - (double)A[i][k] * temp;
+				}
+			}
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n + 1; k++) {
+					cout << A[j][k] << " ";
+				}
+				cout << endl;
+			}
+			cout << endl;
 		}
 	}
 	return A;
 }
 
 int main() {
-	vector<vector<int>> A = { {1, 1, -1}, {0, 1, 3}, {-1, 0, 2} };	
-	vector<int> b = { 9, 3, 2 };
-	vector<vector<int>> B = Elimination(A, b);
-	for (int i = 0; i < A.size(); i++) {
-		for (int k = 0; k < A[i].size(); k++) {
-			cout << B[i][k] << " "; // Prints the matrix
-		}
-		cout << endl;
-	}
+	vector<vector<double>> A = { {1, 1, -1}, {0, 1, 3}, {-1, 0, 2} };	
+	vector<double> b = { 9, 3, 2 };
+	vector<vector<double>> A2 = { {1, 1, 1}, {1, 1, 2}, {2, 2, 3} };
+	vector<double> b2 = { 6, 9, 15 };
+	vector<vector<double>> A3 = { {1,1,1,1,1,1,1,1}, {1,2,1,1,1,1,2,1}, {1,1,3,1,1,3,1,1}, {1,1,1,4,4,1,1,1}, {11,1,1,1,1,1,1,1}, {1,1,1,1,-1,-1,-1,-1}, {1,2,3,4,5,6,7,8}, {1,-1,1,-1,1,-1,1,-1} };
+	vector<double> b3 = { 0,0,0,0,20,34,-51,-6 };
+	vector<vector<double>> B = Elimination(A3, b3);
 }
